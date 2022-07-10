490c16ee2dbbdef32b90c9707473abf0e379b595
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-10 12:07:34 +0300

    Create poster area in detail page and add react grid package

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 
+import { Container, Row, Col } from "react-grid-system";
+
 import { useGetTheMovieDetailApiQuery } from "../store/apis/TheMovieApi";
 
 import DropdownMenu from "../components/DropdownMenu/DropdownMenu";
 
 export default function MovieDetail() {
   const [backgroundImage, setBackgroundImage] = useState("");
+  const imageBackPath = "https://image.tmdb.org/t/p/original";
+  const imagePosterPath = "https://image.tmdb.org/t/p/w500";
   const { data } = useGetTheMovieDetailApiQuery("43539", {
     refetchOnMountOrArgChange: true,
   });
 
   useEffect(() => {
-    setBackgroundImage(
-      `https://image.tmdb.org/t/p/original${data.backdrop_path}`
-    );
-  }, []);
+    setBackgroundImage(`${imageBackPath}${data?.backdrop_path}`);
+  }, [data]);
 
   console.log("backgroundImage", backgroundImage);
 
@@ -78,7 +80,46 @@ export default function MovieDetail() {
           }}
         >
           <div className="detail-container">
-            <div className="detail-block"></div>
+            <div className="detail-block">
+              <Container>
+                <Row>
+                  <Col sm={4}>
+                    <div className="left-block">
+                      <div className="image-block">
+                        <img
+                          src={`${imagePosterPath}${data?.poster_path}`}
+                          alt="Movie Footer"
+                        />
+                      </div>
+                      <div className="text-block">
+                        <div className="image-item">
+                          <picture>
+                            <source
+                              media="(min-width: 641px)"
+                              srcSet="/assets/img/streaming.webp"
+                              type="image/webp"
+                            />
+                            <img
+                              src="/assets/img/streaming.png"
+                              alt="Now Streaming on puhutv"
+                            />
+                          </picture>
+                        </div>
+                        <div className="text-item">
+                          <h4 className="streaming-text">Now Streaming</h4>
+                          <h3 className="streaming-text">Watch Now</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col sm={8}>
+                    <div className="right-block">
+                      <p>DENEME</p>
+                    </div>{" "}
+                  </Col>
+                </Row>
+              </Container>
+            </div>
           </div>
         </div>
       </div>
