a8f2b537c8a67957e0c7fce4e6dfa5a3eed804f8
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-13 00:33:39 +0300

    Add production company logo in detail page

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -2,19 +2,29 @@ import React from "react";
 
 export default function Poster(props) {
   const imagePosterPath = "https://image.tmdb.org/t/p/w500";
+  const propertyNames =
+    props.providersData?.results && Object.keys(props.providersData?.results);
+  const index =
+    propertyNames &&
+    propertyNames.findIndex((element) => {
+      if (element === props.production) {
+        return true;
+      }
+      return false;
+    });
 
-  // const logoPath = props.providersData?.results?.filter(
-  //   (f) => f === props.production
-  // );
-
-  // const arr = [];
-  // Object.keys(props.providersData?.results).forEach((key) =>
-  //   arr.push({ name: key, value: props.providersData?.results[key] })
-  // );
-
-  // console.log("providersData", arr);
-  // console.log("production", props.production);
-  // console.log("logoPath", logoPath);
+  var array =
+    props.providersData?.results &&
+    Object.keys(props.providersData?.results).map(function (key) {
+      return props.providersData?.results[key];
+    });
+  const logoPath =
+    index &&
+    array[index] &&
+    array[index]?.flatrate &&
+    array[index]?.flatrate[0] &&
+    array[index]?.flatrate[0]?.logo_path &&
+    array[index]?.flatrate[0]?.logo_path;
 
   return (
     <div className="left-block">
@@ -22,19 +32,14 @@ export default function Poster(props) {
         <img src={`${imagePosterPath}${props.path}`} alt="Poster" />
       </div>
       <div className="text-block">
-        <div className="image-item">
-          <picture>
-            <source
-              media="(min-width: 641px)"
-              srcSet="/assets/img/streaming.webp"
-              type="image/webp"
-            />
+        {logoPath && (
+          <div className="image-item">
             <img
-              src="/assets/img/streaming.png"
-              alt="Now Streaming on puhutv"
+              src={`${imagePosterPath}${logoPath}`}
+              alt={array[index].flatrate[0].provider_name}
             />
-          </picture>
-        </div>
+          </div>
+        )}
         <div className="text-item">
           <h4 className="streaming-text">Now Streaming</h4>
           <h3 className="streaming-text">Watch Now</h3>
