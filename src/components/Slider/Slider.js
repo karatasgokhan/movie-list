8b342d2fa2dd7a346b87e5b66b6eb6d249addd4e
karatasgokhan <58986991+karatasgokhan@users.noreply.github.com>
2022-07-12 15:02:56 +0300

    Add  react top loading bar

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import LoadingBar from "react-top-loading-bar";
 
 import { useGetTheMoviesAndTvApiQuery } from "../../store/apis/TheMovieApi";
 
@@ -6,18 +8,40 @@ import SliderItem from "./SliderItem";
 import Switch from "../Switch/Switch";
 
 export default function Slider() {
+  const [progress, setProgress] = useState(0);
+  const [visibleBar, setVisibleBar] = useState(false);
   const [selected, setSelected] = useState("movie");
   const { data } = useGetTheMoviesAndTvApiQuery(selected, {
     refetchOnMountOrArgChange: true,
   });
 
+  useEffect(() => {
+    setProgress(99);
+    setVisibleBar(true);
+    setTimeout(() => {
+      setProgress(0);
+      setVisibleBar(false);
+    }, 800);
+  }, [data]);
+
   return (
-    <div className="slider-block">
-      <div className="slider-header">
-        <h2>What's Popular</h2>
-        <Switch setSelected={setSelected} selected={selected} />
+    <>
+      {visibleBar && (
+        <LoadingBar
+          color="#01b4e4"
+          background="transparent"
+          height="4px"
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+      )}
+      <div className="slider-block">
+        <div className="slider-header">
+          <h2>What's Popular</h2>
+          <Switch setSelected={setSelected} selected={selected} />
+        </div>
+        <SliderItem data={data} selected={selected} />
       </div>
-      <SliderItem data={data} selected={selected} />
-    </div>
+    </>
   );
 }
